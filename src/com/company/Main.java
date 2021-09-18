6164dd08a1113582c5dbaa723b0199554101f00a
kaaverr <makciimgogol@gmail.com>
2021-09-18 11:07:12 +0300

    v 1.0.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,17 +3,35 @@ package com.company;
 public class Main {
 
 public static String getPrefix(String[] array){
-    String prefix = array[0].substring(0,1);
-    for (int i = 0; i < array.length ; i++) {
-        if (!prefix.equals(array[i].substring(0,1))){
-
+    String prefix = "";
+    String shortest = getShortest(array);
+    for (int i = 0; i < shortest.length(); i++) {
+        char symvol = shortest.charAt(i);
+        for (int j = 0; j < array.length; j++) {
+            if (symvol != array[j].charAt(i)){
+                return prefix;
+            }
         }
+//        prefix = prefix + symvol;
+        prefix += symvol;
     }
-    return null;
+    return prefix;
+
 }
 
-    public static void main(String[] args) {
-	String[] array = new String[]{"abcde" , "abcd" , "abd" , "abxy" , "abda" , "abc"};
-        System.out.println(getPrefix(array));
+public static String getShortest(String[] array){
+    String shortest = array[0];
+    for (int i = 0; i <array.length; i++) {
+        if (array[i].length() < shortest.length()){
+            shortest = array[i];
+        }
     }
+    return shortest;
+}
+
+public static void main(String[] args) {
+	String[] array = new String[]{"abcde" , "abcd" , "abcb" , "abcxy" , "aba" , "ah"};
+	System.out.println(getPrefix(array));
+//    System.out.println(getShortest(array));
+}
 }
